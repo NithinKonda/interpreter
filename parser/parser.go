e79016eb3e165683146405b1b549b3935099123d
NithinKonda <nithinkonda142@gmail.com>
2024-10-27 20:35:55 +0530

    fix: Add parseBlockStatement

--- parser/parser.go
+++ parser/parser.go
@@ -275,3 +275,17 @@ func (p *Parser) parseIfExpression() ast.Expression {
 	expression.Consequence = p.parseBlockStatement()
 	return expression
 }
+
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.curToken}
+	block.Statements = []ast.Statement{}
+	p.nextToken()
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return block
+}
