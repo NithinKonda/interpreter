bc292314cd4103d17e3e1f602097fb1838a47b75
NithinKonda <nithinkonda142@gmail.com>
2024-11-01 20:10:31 +0530

    feat: Add Bool Null OBJs

--- object/object.go
+++ object/object.go
@@ -13,3 +13,18 @@ type Integer struct {
 }
 
 func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
