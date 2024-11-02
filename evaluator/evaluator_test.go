3f86c7e8acd705c7fb9acccf37eb1186e2ea3163
NithinKonda <nithinkonda142@gmail.com>
2024-11-03 00:40:59 +0530

    fix: Add Eval and statements

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -2,6 +2,7 @@ package evaluator
 
 import (
 	"testing"
+	"writing/ast"
 	"writing/lexer"
 	"writing/object"
 	"writing/parser"
@@ -38,4 +39,24 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 		return false
 	}
 	return true
+}
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
 }
