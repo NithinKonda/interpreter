98508fa3686fb7becc7e4af5fe131edfdfb620fb
NithinKonda <nithinkonda142@gmail.com>
2024-11-07 20:37:21 +0530

    fix: Remove Null types

--- object/object.go
+++ object/object.go
@@ -8,6 +8,19 @@ import (
 )
 
 type ObjectType string
+
+const (
+	NULL_OBJ  = "NULL"
+	ERROR_OBJ = "ERROR"
+
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
+
+	FUNCTION_OBJ = "FUNCTION"
+)
+
 type Object interface {
 	Type() ObjectType
 	Inspect() string
@@ -17,23 +30,8 @@ type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
-
-const (
-	INTEGER_OBJ      = "INTEGER"
-	BOOLEAN_OBJ      = "BOOLEAN"
-	NULL_OBJ         = "NULL"
-	RETURN_VALUE_OBJ = "RETURN_VALUE"
-	ERROR_OBJ        = "ERROR"
-	FUNCTION_OBJ     = "FUNCTION"
-)
-
-type Null struct{}
-
-func (n *Null) Type() ObjectType { return NULL_OBJ }
-func (n *Null) Inspect() string  { return "null" }
-
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 
 type Boolean struct {
 	Value bool
@@ -41,6 +39,11 @@ type Boolean struct {
 
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
 
 type ReturnValue struct {
 	Value Object
