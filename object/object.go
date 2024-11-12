9f5415d494fda9b9f617c2a15118f6df94b8849a
NithinKonda <nithinkonda142@gmail.com>
2024-11-12 21:58:17 +0530

    fea: Add Strings to the Object

--- object/object.go
+++ object/object.go
@@ -19,6 +19,8 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 
 	FUNCTION_OBJ = "FUNCTION"
+
+	STRING_OBJ = "STRING"
 )
 
 type Object interface {
@@ -83,3 +85,10 @@ func (f *Function) Inspect() string {
 
 	return out.String()
 }
+
+type String struct {
+	Value string
+}
+
+func (s *String) Type() ObjectType { return STRING_OBJ }
+func (s *String) Inspect() string  { return s.Value }
